8003e6ace56cd4b6fa42c3d935bd2d380976190c
Kelsey <r.i.kelsey@gmail.com>
2013-10-26 21:54:55 +0100

    Remove gotos
    Code tidy up and remove gotos

--- modules/visualization/glloudness.c
+++ modules/visualization/glloudness.c
@@ -123,7 +123,9 @@ static int Open(vlc_object_t * p_this)
 
     p_sys = p_filter->p_sys = (filter_sys_t*)malloc(sizeof(*p_sys));
     if (p_sys == NULL)
+    {
         return VLC_ENOMEM;
+    }
 
     /* Create the object for the thread */
     vlc_sem_init(&p_sys->ready, 0);
@@ -138,29 +140,26 @@ static int Open(vlc_object_t * p_this)
 
     /* Create the FIFO for the audio data. */
     p_sys->fifo = block_FifoNew();
-    if (p_sys->fifo == NULL)
-        goto error;
-
-    /* Create the thread */
-    if (vlc_clone(&p_sys->thread, Thread, p_filter,
-                  VLC_THREAD_PRIORITY_VIDEO))
-        goto error;
-
-    /* Wait for the displaying thread to be ready. */
-    vlc_sem_wait(&p_sys->ready);
-    if (p_sys->b_error)
+    if (p_sys->fifo != NULL)
     {
-        vlc_join(p_sys->thread, NULL);
-        goto error;
-    }
-
-    p_filter->fmt_in.audio.i_format = VLC_CODEC_FL32;
-    p_filter->fmt_out.audio = p_filter->fmt_in.audio;
-    p_filter->pf_audio_filter = DoWork;
-
-    return VLC_SUCCESS;
-
-error:
+		/* Create the thread */
+		if (!vlc_clone(&p_sys->thread, Thread, p_filter, VLC_THREAD_PRIORITY_VIDEO))
+		{
+			/* Wait for the displaying thread to be ready. */
+			vlc_sem_wait(&p_sys->ready);
+			if (!p_sys->b_error)
+			{
+				p_filter->fmt_in.audio.i_format = VLC_CODEC_FL32;
+				p_filter->fmt_out.audio = p_filter->fmt_in.audio;
+				p_filter->pf_audio_filter = DoWork;
+				return VLC_SUCCESS;
+			}
+			else
+			{
+				vlc_join(p_sys->thread, NULL);
+			}
+		}
+	}
     vlc_sem_destroy(&p_sys->ready);
     free(p_sys);
     return VLC_EGENERIC;
@@ -201,7 +200,9 @@ static block_t *DoWork(filter_t *p_filter, block_t *p_in_buf)
 {
     block_t *block = block_Duplicate(p_in_buf);
     if (likely(block != NULL))
-        block_FifoPut(p_filter->p_sys->fifo, block);
+    {
+    	block_FifoPut(p_filter->p_sys->fifo, block);
+    }
     return p_in_buf;
 }
 
@@ -356,15 +357,17 @@ static void *Thread( void *p_data )
     unsigned int i_last_height = 0;
 
     /* Create the openGL provider */
-    p_sys->p_vout =
-        (vout_thread_t *)vlc_object_create(p_filter, sizeof(vout_thread_t));
+    p_sys->p_vout = (vout_thread_t *)vlc_object_create(p_filter, sizeof(vout_thread_t));
     if (!p_sys->p_vout)
-        goto error;
+    {
+    	p_sys->b_error = true;
+        vlc_sem_post(&p_sys->ready);
+        return NULL;
+    }
 
     /* Configure the video format for the opengl provider. */
     video_format_Init(&fmt, 0);
-    video_format_Setup(&fmt, VLC_CODEC_RGB32,
-                       p_sys->i_width, p_sys->i_height, 0, 1 );
+    video_format_Setup(&fmt, VLC_CODEC_RGB32, p_sys->i_width, p_sys->i_height, 0, 1 );
     fmt.i_sar_num = 1;
     fmt.i_sar_den = 1;
 
@@ -379,12 +382,14 @@ static void *Thread( void *p_data )
     state.sar.num = 1;
     state.sar.den = 1;
 
-    p_sys->p_vd = vout_NewDisplay(p_sys->p_vout, &fmt, &state,
-                                  "opengl", 1000000, 1000000);
+    p_sys->p_vd = vout_NewDisplay(p_sys->p_vout, &fmt, &state, "opengl", 1000000, 1000000);
     if (!p_sys->p_vd)
     {
         vlc_object_release(p_sys->p_vout);
-        goto error;
+
+        p_sys->b_error = true;
+        vlc_sem_post(&p_sys->ready);
+        return NULL;
     }
 
     gl = vout_GetDisplayOpengl(p_sys->p_vd);
@@ -392,7 +397,10 @@ static void *Thread( void *p_data )
     {
         vout_DeleteDisplay(p_sys->p_vd, NULL);
         vlc_object_release(p_sys->p_vout);
-        goto error;
+
+        p_sys->b_error = true;
+        vlc_sem_post(&p_sys->ready);
+        return NULL;
     }
 
     vlc_sem_post(&p_sys->ready);
@@ -409,21 +417,18 @@ static void *Thread( void *p_data )
 
         /* Manage the events */
         vout_ManageDisplay(p_sys->p_vd, true);
-        if (p_sys->p_vd->cfg->display.width != i_last_width ||
-            p_sys->p_vd->cfg->display.height != i_last_height)
+        if (p_sys->p_vd->cfg->display.width != i_last_width || p_sys->p_vd->cfg->display.height != i_last_height)
         {
             /* FIXME it is not perfect as we will have black bands */
             vout_display_place_t place;
-            vout_display_PlacePicture(&place, &p_sys->p_vd->source,
-                                      p_sys->p_vd->cfg, false);
+            vout_display_PlacePicture(&place, &p_sys->p_vd->source, p_sys->p_vd->cfg, false);
 
             i_last_width  = p_sys->p_vd->cfg->display.width;
             i_last_height = p_sys->p_vd->cfg->display.height;
         }
 
         /* Horizontal scale for 20-band equalizer */
-        const unsigned xscale[] = {0,1,2,3,4,5,6,7,8,11,15,20,27,
-                                   36,47,62,82,107,141,184,255};
+        const unsigned xscale[] = {0,1,2,3,4,5,6,7,8,11,15,20,27,36,47,62,82,107,141,184,255};
 
         fft_state *p_state; /* internal FFT data */
 
@@ -441,11 +446,14 @@ static void *Thread( void *p_data )
         if (block->i_nb_samples != p_sys->i_prev_nb_samples)
         {
             free(p_sys->p_prev_s16_buff);
-            p_sys->p_prev_s16_buff = malloc(block->i_nb_samples *
-                                            p_sys->i_channels *
-                                            sizeof(int16_t));
+            p_sys->p_prev_s16_buff = malloc(block->i_nb_samples * p_sys->i_channels * sizeof(int16_t));
             if (!p_sys->p_prev_s16_buff)
-                goto release;
+            {
+            	block_Release(block);
+            	vlc_restorecancel(canc);
+                continue;
+            }
+
             p_sys->i_prev_nb_samples = block->i_nb_samples;
         }
         p_buffs = p_s16_buff = p_sys->p_prev_s16_buff;
@@ -458,20 +466,30 @@ static void *Thread( void *p_data )
 
             u.f = *p_buffl + 384.0;
             if (u.i > 0x43c07fff)
+            {
                 *p_buffs = 32767;
+            }
             else if (u.i < 0x43bf8000)
-                *p_buffs = -32768;
+            {
+            	*p_buffs = -32768;
+            }
             else
-                *p_buffs = u.i - 0x43c00000;
+            {
+            	*p_buffs = u.i - 0x43c00000;
+            }
 
             p_buffl++; p_buffs++;
         }
+
         p_state = visual_fft_init();
         if (!p_state)
         {
             msg_Err(p_filter,"unable to initialize FFT transform");
-            goto release;
+            block_Release(block);
+            vlc_restorecancel(canc);
+            continue;
         }
+
         p_buffs = p_s16_buff;
         for (i = 0 ; i < FFT_BUFFER_SIZE; i++)
         {
@@ -480,20 +498,27 @@ static void *Thread( void *p_data )
 
             p_buffs += p_sys->i_channels;
             if (p_buffs >= &p_s16_buff[block->i_nb_samples * p_sys->i_channels])
+            {
                 p_buffs = p_s16_buff;
+            }
         }
+
         fft_perform (p_buffer1, p_output, p_state);
 
-        for (i = 0; i< FFT_BUFFER_SIZE; ++i)
-            p_dest[i] = p_output[i] *  (2 ^ 16)
-                        / ((FFT_BUFFER_SIZE / 2 * 32768) ^ 2);
+        for (i = 0; i < FFT_BUFFER_SIZE; ++i)
+        {
+        	const float FFT_MAX_VALUE = (FFT_BUFFER_SIZE / 2 * 32768) ^ 2;
+            p_dest[i] = p_output[i] *  (2 ^ 16) / FFT_MAX_VALUE;
+        }
 
         for (i = 0 ; i < NB_BANDS; i++)
         {
             /* Decrease the previous size of the bar. */
             height[i] -= BAR_DECREMENT;
             if (height[i] < 0)
+            {
                 height[i] = 0;
+            }
 
             int y = 0;
             /* We search the maximum on one scale
@@ -501,27 +526,32 @@ static void *Thread( void *p_data )
             for (j = xscale[i]; j < xscale[i + 1]; j++)
             {
                 if (p_dest[j] > y)
+                {
                      y = p_dest[j];
+                }
             }
             /* Calculate the height of the bar */
             float new_height = y != 0 ? log(y) * 0.4 : 0;
-            height[i] = new_height > height[i]
-                        ? new_height : height[i];
+            height[i] = new_height > height[i] ? new_height : height[i];
         }
 
         /* Determine the camera rotation angle. */
         p_sys->f_rotationAngle += p_sys->f_rotationIncrement;
         if (p_sys->f_rotationAngle <= -ROTATION_MAX)
+        {
             p_sys->f_rotationIncrement = ROTATION_INCREMENT;
+        }
         else if (p_sys->f_rotationAngle >= ROTATION_MAX)
-            p_sys->f_rotationIncrement = -ROTATION_INCREMENT;
+        {
+        	p_sys->f_rotationIncrement = -ROTATION_INCREMENT;
+        }
 
         /* Render the frame. */
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glPushMatrix();
-            glRotatef(p_sys->f_rotationAngle, 0, 1, 0);
-            drawBars(height);
+        glRotatef(p_sys->f_rotationAngle, 0, 1, 0);
+        drawBars(height);
         glPopMatrix();
 
         /* Wait to swapp the frame on time. */
@@ -532,16 +562,12 @@ static void *Thread( void *p_data )
             vlc_gl_Unlock(gl);
         }
 
-release:
         block_Release(block);
         vlc_restorecancel(canc);
     }
 
     assert(0);
-
-error:
     p_sys->b_error = true;
     vlc_sem_post(&p_sys->ready);
     return NULL;
 }
-
